ee57f887693dd8e9bf8af2b8bd85ba806fd47d5b
Ryan Kim <nainirajk@gmail.com>
2014-10-27 21:19:50 +0900

    finish chap8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
 	user = User.find_by(email: params[:session][:email].downcase)
 	if user && user.authenticate(params[:session][:password])
 		log_in user
-      # remember user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    # remember user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 		redirect_to user
 		# Log the use rin and redirect to the user's show page.
 	else
